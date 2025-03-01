3e487e2ab84eadef9ab35cd71df5d4bf8af906a9
daniel-svendsen <daniel-svendsen@hotmail.se>
2025-03-01 15:04:01 +0100

    changing to cloudflare

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,52 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import sitemap from 'vite-plugin-sitemap';
-import path from 'path';
-import visualizer from "rollup-plugin-visualizer";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import sitemap from 'vite-plugin-sitemap'
+import path from 'path'
+import visualizer from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-    plugins: [
-        react(),
-        tsconfigPaths(),
-        sitemap({
-            hostname: 'https://www.svendsenphotography.com',
-            dynamicRoutes: [
-                '/services',
-                '/weddings',
-                '/portraits',
-                '/faq',
-                '/contact',
-                '/work',
-            ],
-            outDir: 'docs',
-            readable: true,
-        }),
-        visualizer.default({
-            filename: 'bundle-analysis.html',
-            open: true,
-        }),
-    ],
-    base: '/',
-    build: {
-        outDir: 'docs',
-        emptyOutDir: true,
-        chunkSizeWarningLimit: 1000,
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        if (id.includes('react')) return 'vendor-react';
-                        if (id.includes('react-router')) return 'vendor-router';
-                        return 'vendor';
-                    }
-                },
-            },
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    sitemap({
+      hostname: 'https://www.svendsenphotography.com',
+      dynamicRoutes: [
+        '/services',
+        '/weddings',
+        '/portraits',
+        '/faq',
+        '/contact',
+        '/work',
+      ],
+      outDir: 'docs',
+      readable: true,
+    }),
+    visualizer.default({
+      filename: 'bundle-analysis.html',
+      open: true,
+    }),
+  ],
+  base: '/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor-react'
+            if (id.includes('react-router')) return 'vendor-router'
+            return 'vendor'
+          }
         },
+      },
     },
-    resolve: {
-        alias: {
-            '@': path.resolve('src'),
-        },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve('src'),
     },
-});
+  },
+})
