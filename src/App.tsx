6919818d5490f19960f0fb89777598e9c29612a5
daniel-svendsen <daniel-svendsen@hotmail.se>
2025-03-05 09:45:00 +0100

    changing back to BrowserRouter + inactivates deploy.yml

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import Navbar from './components/Navbar'
 import ScrollToTop from './components/ScrollToTop'
